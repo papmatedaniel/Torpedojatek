bcaa4e6971213df66dbd9ce217ec97cac07784fd
papmatedaniel <90318564+papmatedaniel@users.noreply.github.com>
2024-12-24 14:07:06 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,3 +1,4 @@
+import os
 class Torpedeo:
     def __init__(self, tabla):
         self.tabla = [["A)", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
@@ -64,11 +65,35 @@ class Torpedeo:
         self.hajokoordinatai = set()
         self.hajok_melletti_koordinatak = set()
 
-    def hajo_bekeres(self):
+    def tabla_megjelenites(self, tabla):
+        "Megjeleníti a táblát"
+        [print(*j) for j in tabla]
+
+    def hajo_bekeres(self, hossz):
         "Bekéri a játékostól a hajókat"
-        print("Add meg a hajó kezdő koordinátáját (A-J-ig és 0-9-ig Pl A0 vagy J9) és az irányt (É/K/NY/D):")
-        adat = input("Kezdő koordináta és irány: ").upper()
-        return adat
+        print("Add meg a hajó kezdő koordinátáját (A-J-ig és 0-9-ig Pl A0 vagy J9)  az irányt (É/K/NY/D):")
+        kezdokordinata = input("Kezdő koordináta: ").upper()
+        if hossz > 1:
+            print("Add meg a hajó irányt (É/K/NY/D):")
+            irany = input("Irány: ").upper()
+            return kezdokordinata, irany
+        return kezdokordinata
+        
+    def iranyellenorzo(self, irany):
+        "Ez a függvény, ellenőrzi, hogy amennyiben szükséges megadni irányt, jól adta e meg az irányt a felhasználó."
+        match irany.upper():
+            case "É":
+                return True
+            case "D":
+                return True
+            case "NY":
+                return True
+            case "K":
+                return True
+            case "NINCS":
+                return True
+            case other:
+                return False
 
     def sor_oszlop_ellenorzes(self, sor, oszlop):
         "Ellenőrzi, hogy a kezdő koordináta pályán belül van e"
@@ -110,9 +135,9 @@ class Torpedeo:
     def utkozes_ellenorzo(self, hajokoordinatak):
         "Ellenőrzi, hogy nem e ütközik más hajóval/nincs mellette közvetlen más hajó"
         hajokoordinatak = set(hajokoordinatak) 
-        if len(hajokoordinatak & self.hajokoordinatai) > 0:
+        if hajokoordinatak & self.hajokoordinatai:
             return False
-        if len(hajokoordinatak & self.hajok_melletti_koordinatak) > 0:
+        if hajokoordinatak & self.hajok_melletti_koordinatak:
             return False
         return True
 
@@ -126,18 +151,22 @@ class Torpedeo:
 
     def hajoellenorzes(self, sor, oszlop, hossz, irany=None):
         "Ellenőrzi a megadott hajót, ha nem felel meg, hibát dob, ellenben vissza adja a hajó koordinátáit."
-        hajokoordinatak = self.kiszamol(sor, oszlop, hossz, irany)
         if not self.sor_oszlop_ellenorzes(sor, oszlop):
             raise ValueError("A kezdő koordináta érvénytelen! Csak A-J és 0-9 értékek engedélyezettek.")
+
+        elif not self.iranyellenorzo(irany):
+            raise ValueError("Az irány érvénytelen! Csak É/D/K/NY engedélyezettek.")
+
+        else:
+            hajokoordinatak = self.kiszamol(sor, oszlop, hossz, irany)        
         
-        
-        elif not self.palyan_beluli_ellenorzes(hajokoordinatak):
-            raise ValueError("A hajó egy vagy több része a pályán kívülre esik!")
-        
-        elif not self.utkozes_ellenorzo(hajokoordinatak):
-            raise ValueError("A hajó ütközik egy másik hajóval, vagy túl közel van hozzá.")
-        
-        return hajokoordinatak
+            if not self.palyan_beluli_ellenorzes(hajokoordinatak):
+                raise ValueError("A hajó egy vagy több része a pályán kívülre esik!")
+            
+            if not self.utkozes_ellenorzo(hajokoordinatak):
+                raise ValueError("A hajó ütközik egy másik hajóval, vagy túl közel van hozzá.")
+            
+            return hajokoordinatak
 
     def hajoelhelyezes(self, hajokoordinatak):
         "Ez a függvény elhelyezi a táblán az új hajót"
@@ -145,51 +174,38 @@ class Torpedeo:
             x,y = int(i[1])+1, ord(i[0]) - ord('A')
             self.tabla[y][x] = "X"
             
-
     def hajokord_es_hajokordmelletti(self, hajokoordinatak):
         "Rögzíti az új hajó koordinátáját és a mellette lévő koordinátákat"
         self.hajok_melletti_koordinatak.update(self.hajo_melletti_kordinatak(hajokoordinatak))
         self.hajokoordinatai.update(hajokoordinatak)
         
-
     def hajoklistamodosito(self, hanyadikhajo, hajokoordinatak):
         "Itt módosítjuk az összes hajó tulajdonságait, pl belerakjuk a koordinatait, és a mellette lévő koordinátákat"
         self.hajoklista[hanyadikhajo-1]["koordinatak"] = hajokoordinatak
         self.hajoklista[hanyadikhajo-1]["mellettekord."] = self.hajo_melletti_kordinatak(hajokoordinatak)
 
 
-player1 = Torpedeo(None)
+player1 = Torpedeo()
 
-# Hajók elhelyezése
 for i, hajo in enumerate(player1.hajoklista, start=1):  # Hajók listájának végigjárása
     while True:  # Addig ismétel, amíg sikeresen el nem helyezi a hajót
         try:
+            player1.tabla_megjelenites(player1.tabla)
             print(f"\nHelyezd el a(z) {i}. hajót! Hossz: {hajo['hajoHossz']} mező")
-            adat = player1.hajo_bekeres()
+            adat = player1.hajo_bekeres(hajo['hajoHossz'])
+    
+            irany = (adat[1] if hajo['hajoHossz'] != 1 else "Nincs")
+            kezdosor = (adat[0][0] if hajo['hajoHossz'] != 1 else adat[0])
+            kezdoszlop = (adat[0][1] if hajo['hajoHossz'] != 1 else adat[1])
             
-            # Bevitt adat feldolgozása
-            kezdosor, kezdoszlop = adat[0], adat[1]#itt hiba ha csak 1 hosszú a hajó!!!
-            if hajo['hajoHossz'] != 1:
-               irany = adat.split()[1]
-            
-            # Hajó koordinátáinak ellenőrzése
             hajokoordinatak = player1.hajoellenorzes(kezdosor, kezdoszlop, hajo["hajoHossz"], irany)
-            
-            # Hajó elhelyezése
             player1.hajoelhelyezes(hajokoordinatak)
-            
-            # Hajók koordinátáinak és mellékkoordinátáinak mentése
             player1.hajokord_es_hajokordmelletti(hajokoordinatak)
-            
-            # Hajó lista frissítése
             player1.hajoklistamodosito(i, hajokoordinatak)
             
-            # Sikeres elhelyezés után kilépés a ciklusból
+            os.system("cls")
             print("Hajó sikeresen elhelyezve!")
-            for i in player1.tabla:
-                print(*i)
             break
         except ValueError as e:
-            # Ha bármelyik ellenőrzés hibát jelez, újra bekérjük az adatokat
+            os.system("cls")
             print(f"Hiba: {e}")
-
