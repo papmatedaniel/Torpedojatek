1f7e8e421a9c6f6fe7f36b72d7d35bda75189d41
papmatedaniel <90318564+papmatedaniel@users.noreply.github.com>
2024-12-26 17:35:03 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -12,6 +12,17 @@ class Torpedeo:
                       ["I)", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
                       ["J)", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
                       ["  ", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]]
+        self.titoktabla = [["A)", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                      ["B)", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                      ["C)", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                      ["D)", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                      ["E)", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                      ["F)", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                      ["G)", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                      ["H)", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                      ["I)", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                      ["J)", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                      ["  ", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]]
         self.hajoklista = [{
                                 "ennyiVan": 0,
                                 "hajoHossz": 4,
@@ -64,21 +75,26 @@ class Torpedeo:
         self.letett_hajok_szama = sum(1 for i in self.hajoklista if i["ennyiVan"] == 1)
         self.hajokoordinatai = set()
         self.hajok_melletti_koordinatak = set()
+        self.tippelt_cellak = []
+        self.eltalalt_hajok_szama = 0
+        self.nev = ""
 
     def tabla_megjelenites(self, tabla):
         "Megjeleníti a táblát"
         [print(*j) for j in tabla]
 
-    def hajo_bekeres(self, hossz):
-        "Bekéri a játékostól a hajókat"
-        print("Add meg a hajó kezdő koordinátáját (A-J-ig és 0-9-ig Pl A0 vagy J9)  az irányt (É/K/NY/D):")
+    def hajo_bekeres(self):
+        "Bekéri a játékostól a hajó kezdőkoordinátáját"
+        print("Add meg a hajó kezdő koordinátáját (A-J-ig és 0-9-ig Pl A0 vagy J9) ")
         kezdokordinata = input("Kezdő koordináta: ").upper()
-        if hossz > 1:
-            print("Add meg a hajó irányt (É/K/NY/D):")
-            irany = input("Irány: ").upper()
-            return kezdokordinata, irany
         return kezdokordinata
-        
+
+    def irany_bekeres(self):
+        "Bekéri a játékostól az irányt"
+        print("Add meg a hajó irányt (É/K/NY/D):")
+        irany = input("Irány: ").upper()
+        return irany
+
     def iranyellenorzo(self, irany):
         "Ez a függvény, ellenőrzi, hogy amennyiben szükséges megadni irányt, jól adta e meg az irányt a felhasználó."
         match irany.upper():
@@ -89,8 +105,6 @@ class Torpedeo:
             case "NY":
                 return True
             case "K":
-                return True
-            case "NINCS":
                 return True
             case other:
                 return False
@@ -149,8 +163,12 @@ class Torpedeo:
                 return False
         return True
 
-    def hajoellenorzes(self, sor, oszlop, hossz, irany=None):
+    def hajoellenorzes(self, kezdokoordinata, hossz, irany=None):
         "Ellenőrzi a megadott hajót, ha nem felel meg, hibát dob, ellenben vissza adja a hajó koordinátáit."
+        if len(kezdokoordinata) != 2:
+            raise ValueError("A kezdőkoordináta csak 2 elemből állhat egy betűből(A-J) és egy számból (0-9)")
+        sor = kezdokoordinata[0]
+        oszlop = kezdokoordinata[1]
         if not self.sor_oszlop_ellenorzes(sor, oszlop):
             raise ValueError("A kezdő koordináta érvénytelen! Csak A-J és 0-9 értékek engedélyezettek.")
 
@@ -184,28 +202,104 @@ class Torpedeo:
         self.hajoklista[hanyadikhajo-1]["koordinatak"] = hajokoordinatak
         self.hajoklista[hanyadikhajo-1]["mellettekord."] = self.hajo_melletti_kordinatak(hajokoordinatak)
 
+    def tippbekero(self, nev):
+        "Bekéri a tippeket"
+        tipp = input(f"{nev}: Hova lősz?: ").upper()
+        return tipp
 
-player1 = Torpedeo()
+    def  tippellenorzo(self, tipp, tippjeid):
+        "Ellenőrzi a tippet, visszatér az eredménnyel"
+        if len(tipp) != 2:
+            raise ValueError("2 elemből kell állnia a tippnek, egy betűből (A-J) és egy számól (0-9)")
+        if not self.sor_oszlop_ellenorzes(tipp[0].upper(), tipp[1].upper()):
+            raise ValueError("A-J között és 0-9 között add meg a koordinátát, pl A0 vagy J9")
+        if tipp in tippjeid:
+            raise ValueError("Ezt már tippelted...")
+        return True
 
-for i, hajo in enumerate(player1.hajoklista, start=1):  # Hajók listájának végigjárása
-    while True:  # Addig ismétel, amíg sikeresen el nem helyezi a hajót
-        try:
-            player1.tabla_megjelenites(player1.tabla)
-            print(f"\nHelyezd el a(z) {i}. hajót! Hossz: {hajo['hajoHossz']} mező")
-            adat = player1.hajo_bekeres(hajo['hajoHossz'])
-    
-            irany = (adat[1] if hajo['hajoHossz'] != 1 else "Nincs")
-            kezdosor = (adat[0][0] if hajo['hajoHossz'] != 1 else adat[0])
-            kezdoszlop = (adat[0][1] if hajo['hajoHossz'] != 1 else adat[1])
+    def hajomodositasok(self, tipp, egyik, masik):
+        "Módodosítja azt amit kell"
+        egyik.tippelt_cellak.append(tipp)
+        x,y = int(tipp[1])+1, ord(tipp[0]) - ord('A')
+        if tipp in masik.hajokoordinatai:
+            for i in masik.hajoklista:
+                if tipp in i["koordinatak"]:
+                    i["eltaltkoordinatak"].append(tipp)
+                    masik.titoktabla[y][x] = "X"
+                    szoveg = f"""\033[32m
+Eltaláltad, lövés koordinata: {tipp}
+Az eltálalált hajó hossza: {i["hajoHossz"]}
+Ennyit találtál el belőle: {len(i["eltaltkoordinatak"])}
+Ezeket a koordinátákat találtad el: {i["eltaltkoordinatak"]}\033[0m
+                            """
+                    if len(i["koordinatak"]) == len(i["eltaltkoordinatak"]):
+                        egyik.eltalalt_hajok_szama += 1
+                        szoveg += "\033[32mTalált süllyedt. \nA teljes hajó megsemmisült \033[0m"
+                    self.tablakmegjelenitese(egyik.titoktabla, masik.titoktabla, egyik.nev, masik.nev)
+                    return szoveg
+        else:
+            masik.titoktabla[y][x] = "#"
+            self.tablakmegjelenitese(egyik.titoktabla, masik.titoktabla, egyik.nev, masik.nev)
+            return "Nem talált"
+
+    def tablakmegjelenitese(self, tabla1, tabla2, nev1, nev2):
+        "Megjeleníti a táblákat, és a hozzájuk tartozó neveket"
+        print(f"{nev1}\t\t\t\t\t{nev2}")
+        for k, v in zip(tabla1, tabla2):
+            print(*k, "            ", *v)
+        
+
+player1 = Torpedeo(None)
+player2 = Torpedeo(None)
+playerek = [player1, player2]
+for player in playerek:
+    os.system("cls")
+    player.nev = input("Add meg a neved: ")
+    player.tabla_megjelenites(player.tabla)
+    for i, hajo in enumerate(player.hajoklista, start=1):  # Hajók listájának végigjárása
+        while True:  # Addig ismétel, amíg sikeresen el nem helyezi a hajót
+            try:
+                # player.tabla_megjelenites(player.tabla)
+                print(f"{player.nev} add meg a hajóid")
+                print(f"\nHelyezd el a(z) {i}. hajót! Hossz: {hajo['hajoHossz']} mező")
+                kezdokoordinata = player.hajo_bekeres()        
+                if hajo['hajoHossz'] != 1: 
+                    irany = player.irany_bekeres() 
+                hajokoordinatak = player.hajoellenorzes(kezdokoordinata, hajo["hajoHossz"], irany)
+                player.hajoelhelyezes(hajokoordinatak)
+                player.hajokord_es_hajokordmelletti(hajokoordinatak)
+                player.hajoklistamodosito(i, hajokoordinatak)
+                
+                os.system("cls")
+                player1.tabla_megjelenites(player.tabla)
+                print("\033[32m Hajó sikeresen elhelyezve! \033[0m")
+                break
+            except ValueError as e:
+                os.system("cls")
+                player1.tabla_megjelenites(player.tabla)
+                print(f"\033[31m Hiba: {e} \033[0m")
+os.system("cls")
+player1.tablakmegjelenitese(player1.titoktabla, player2.titoktabla, player1.nev, player2.nev)
+jatekosvalaszto = 0
+while True:
+    egyik = playerek[jatekosvalaszto % 2]
+    masik = playerek[(jatekosvalaszto % 2)-1]
+    try:
+        while True:
             
-            hajokoordinatak = player1.hajoellenorzes(kezdosor, kezdoszlop, hajo["hajoHossz"], irany)
-            player1.hajoelhelyezes(hajokoordinatak)
-            player1.hajokord_es_hajokordmelletti(hajokoordinatak)
-            player1.hajoklistamodosito(i, hajokoordinatak)
-            
-            os.system("cls")
-            print("Hajó sikeresen elhelyezve!")
-            break
-        except ValueError as e:
-            os.system("cls")
-            print(f"Hiba: {e}")
+            tipp = egyik.tippbekero(egyik.nev)
+            if egyik.tippellenorzo(tipp, egyik.tippelt_cellak):
+                os.system("cls")
+                print(egyik.hajomodositasok(tipp, egyik, masik))
+                jatekosvalaszto += 1
+                break
+    except ValueError as e:
+        os.system("cls")
+        egyik.tablakmegjelenitese(player1.titoktabla, player2.titoktabla, player1.nev, player2.nev)
+        print(f"\033[31m Hiba: {e} \033[0m")
+
+    if egyik.eltalalt_hajok_szama == 6:
+        print(f"\033[31mA győztes: {egyik.nev}\033[0m")
+        print("ITT voltak a hajók:")
+        egyik.tablakmegjelenitese(player1.tabla, player2.tabla, player1.nev, player2.nev)
+        break
